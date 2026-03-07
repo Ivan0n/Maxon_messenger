575dd05635ea53fb55607eb2688f08fcfa458c5c
ZastyK <vano19082@gmail.com>
2026-03-07 14:36:50 +0300

    VisualStudio Trash

---
+++ Maxon_messenger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maxon_messenger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
