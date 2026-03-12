9614f8905c2540b9fb6fe08934c3caf13a8a52c9
ZastyK <vano19082@gmail.com>
2026-03-12 18:59:10 +0300

    #1 Починка прикола с файлами, #2 начало

--- Maxon_messenger/Program.cs
+++ Maxon_messenger/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Text;
+
 
 namespace Maxon_messenger
 {
     internal class Program
     {
+        private static HttpListener _listener;
+        private const string Url = "http://localhost:8080/";
         static void Main(string[] args)
         {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add(Url);
+            _listener.Start();
+
+            Console.WriteLine($"Сервер запущен: {Url}");
+            Console.WriteLine("Нажмите Ctrl+C для остановки.\n");
+            while (true)
+            {
+                HttpListenerContext context = _listener.GetContext();
+                HandleRequest(context);
+            }
         }
     }
 }
